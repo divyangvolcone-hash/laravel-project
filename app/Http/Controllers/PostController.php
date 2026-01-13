a1899b0f66739e4a79ae9caf62ec4304e825abb0
divyangvolcone-hash <1921324+divyang19@users.noreply.github.com>
2026-01-14 00:38:23 +0530

    Post Controller Updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -66,7 +66,7 @@ class PostController extends Controller
         $post->update($data);
         return redirect()->route('posts.index');
     }
-
+ 
     public function store(Request $request)
     {
         $data = $request->validate([
